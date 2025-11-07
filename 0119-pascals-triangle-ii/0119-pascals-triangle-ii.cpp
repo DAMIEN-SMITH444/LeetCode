7392de08d44c9957a0b3fb0f59a58ddb1775dc4d
DAMIEN_SMITH <155760727+DAMIEN-SMITH444@users.noreply.github.com>
2025-11-07 15:48:59 +0530

    Time: 0 ms (100%), Space: 8.9 MB (52.03%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int ncr(int r , int c){
+        long long res = 1;
+        for(int i = 0; i < c; i++){
+            res = res* (r-i);
+            res = res / (i+1);
+        }
+        return res;
+    }
+    vector<int> getRow(int rowIndex) {
+         vector<int> ans;
+           for(int i = 0;i<=rowIndex;i++){
+               ans.push_back(ncr( (rowIndex) , i ));
+           }
+           return ans;
+    }
+};
